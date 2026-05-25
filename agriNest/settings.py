95c0f1220f0c650c4d09a182aeda7df9c8558416
shojib353 <shojib.shojib10@gmail.com>
2026-05-25 22:27:02 +0600

    error solve

--- agriNest/settings.py
+++ agriNest/settings.py
@@ -261,20 +261,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-# # Media files (uploaded images)
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / "media"
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Media files (uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 
 
 # Default primary key field type
